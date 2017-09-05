49c98a94623112884f205039a7375b2015d0102d
kiongtagious <kionglao20@gmai.com>
2017-09-05 08:18:05 -0500

    Created AddAnimal Method

---
+++ Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    public class Animal
+    {
+        string newAnimal;
+        
+        public Animal()
+        {
+
+        }
+        public void AddAnimal()
+        {
+            List<Dog> name = new List<Dog>();
+            string [] newname = new string[] {""}
+        }
+    }
+}
